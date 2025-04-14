455f6308b89f3ecc527734d392e2a2798dbd06d0
PedroHenriqueLorenzi <168107777+PedroHenriqueLorenzi@users.noreply.github.com>
2025-04-14 13:29:27 -0300

    update

---
+++ documento.py
@@ -0,0 +1,9 @@
+#documento
+
+class Documento:
+    def __init__(self, ide: int, titulo: str, descricao: str, data_envio: int, autor):
+        self.__ide = ide
+        self.__titulo = titulo
+        self.__descricao = descricao
+        self.__data_envio = data_envio
+        self.__autor = autor
