ecaf75bd53fe8dbe9d3ffd2c7d5a475645cf564d
Pedro Henrique Lorenzi Martins <pedro.henrique.loren@gmail.com>
2025-05-25 04:31:28 -0300

    !@#$%¨&&*(¹¹¹¹¹¹¹¹²³££££

--- src/module/usuario/advogado.py
+++ src/module/usuario/advogado.py
@@ -8,8 +8,33 @@ class Advogado(Usuario):
         self.__oab = oab
         self.__processos = []
 
+    @property
+    def ide(self):
+        return self.__ide
+    @ide.setter
+    def ide(self, nova_ide):
+        if isinstance(nova_ide, int): self.__ide = nova_ide
 
-    #getters e setters...
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self, novo_nome):
+        if isinstance(novo_nome, str): self.__nome = novo_nome
+
+    @property
+    def cpf(self):
+        return self.__cpf
+    @cpf.setter
+    def cpf(self, novo_cpf):
+        if isinstance(novo_cpf, int): self.__cpf = novo_cpf
+
+    @property
+    def data_nascimento(self):
+        return self.__data_nascimento
+    @data_nascimento.setter
+    def data_nascimento(self, nova_data_nascimento):
+        if isinstance(nova_data_nascimento, str): self.__data_nascimento = nova_data_nascimento
 
 
     def registrar_movimentacao(self, processo, movimentacao):
