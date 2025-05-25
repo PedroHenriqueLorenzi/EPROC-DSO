9ebe909db2d3d40176896c3891ae7b857b9eeac3
Pedro Henrique Lorenzi Martins <pedro.henrique.loren@gmail.com>
2025-05-25 13:34:01 -0300

    a

--- src/module/processo.py
+++ src/module/processo.py
@@ -24,7 +24,7 @@ class Processo:
         self.__promotor_responsavel = promotor_responsavel
         self.__juiz_responsavel = juiz_responsavel
         self.__arquivamento = None
-        self.__processo_arquivado = False
+        self.__processo_encerrado = False
         self.__advogados = []
         self.__partes = []
         self.__documentos = []
@@ -52,44 +52,44 @@ class Processo:
     @.setter
 
     def adicionar_reu(self, reu):
-        if isinstance(reu, Reu) and not self.__processo_arquivado: self.__partes.append(reu)
+        if isinstance(reu, Reu) and not self.__processo_encerrado: self.__partes.append(reu)
 
     def adicionar_vitima(self, vitima):
-        if isinstance(vitima, Vitima) and not self.__processo_arquivado: self.__partes.append(vitima)
+        if isinstance(vitima, Vitima) and not self.__processo_encerrado: self.__partes.append(vitima)
 
     def adicionar_advogado(self, advogado):
-        if isinstance(advogado, Advogado) and not self.__processo_arquivado: self.__advogados.append(advogado)
+        if isinstance(advogado, Advogado) and not self.__processo_encerrado: self.__advogados.append(advogado)
 
     def trocar_juiz_responsavel(self, juiz): ####
-        if isinstance(juiz, Juiz) and not self.__processo_arquivado: self.__juiz_responsavel = juiz
+        if isinstance(juiz, Juiz) and not self.__processo_encerrado: self.__juiz_responsavel = juiz
 
-    def trocar_promotor_responsavel(self, promotor): ###
-        if isinstance(promotor, Promotor) and not self.__processo_arquivado: self.__promotor_responsavel = promotor
+    def trocar_promotor_responsavel(self, promotor): ####
+        if isinstance(promotor, Promotor) and not self.__processo_encerrado: self.__promotor_responsavel = promotor
 
     def trocar_tribunal_responsavel(self, tribunal): ####
-        if isinstance(tribunal, Tribunal) and not self.__processo_arquivado: self.__tribunal_responsavel = tribunal
+        if isinstance(tribunal, Tribunal) and not self.__processo_encerrado: self.__tribunal_responsavel = tribunal
 
 
 
 
     def adicionar_acusacao(self, acusacao):
-        if isinstance(acusacao, Acusacao) and not self.__processo_arquivado: self.__documentos.append(acusacao)
+        if isinstance(acusacao, Acusacao) and not self.__processo_encerrado: self.__documentos.append(acusacao)
 
     def adicionar_audiencia(self, audiencia):
-        if isinstance(audiencia, Audiencia) and not self.__processo_arquivado: self.__documentos.append(audiencia)
+        if isinstance(audiencia, Audiencia) and not self.__processo_encerrado: self.__documentos.append(audiencia)
 
     def adicionar_defesa(self, defesa):
-        if isinstance(defesa, Defesa) and not self.__processo_arquivado: self.__documentos.append(defesa)
+        if isinstance(defesa, Defesa) and not self.__processo_encerrado: self.__documentos.append(defesa)
 
     #def registrar_movimentacao(self, movimentacao):
         #pass
 
     def adicionar_sentenca(self, sentenca):
-        if isinstance(sentenca, Sentenca) and not self.__processo_arquivado: self.__documentos.append(sentenca)
+        if isinstance(sentenca, Sentenca) and not self.__processo_encerrado: self.__documentos.append(sentenca)
 
 
 
     def adicionar_arquivamento(self, arquivamento):
-        if isinstance(arquivamento, Arquivamento) and not self.__processo_arquivado:
+        if isinstance(arquivamento, Arquivamento) and not self.__processo_encerrado:
             self.__arquivamento.append(arquivamento)
-            self.__processo_arquivado = True
+            self.__processo_encerrado = True
