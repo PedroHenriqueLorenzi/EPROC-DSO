e20d76db501ae70341abbb3a1d9b28f1e58d2632
Pedro Henrique Lorenzi Martins <pedro.henrique.loren@gmail.com>
2025-05-25 04:08:46 -0300

    Update abstractDocumento.py

--- src/module/documento/abstractDocumento.py
+++ src/module/documento/abstractDocumento.py
@@ -2,8 +2,8 @@ from abc import ABC,abstractmethod
 from src.module.usuario.abstractUsuario import Usuario
 class Documento(ABC):
     @abstractmethod
-    def __init__(self, id: int, titulo: str, descricao: str, data_envio: str, autor: Usuario):
-        self.__id = id
+    def __init__(self, ide: int, titulo: str, descricao: str, data_envio: str, autor: Usuario):
+        self.__ide = ide
         self.__titulo = titulo
         self.__descricao = descricao
         self.__data_envio = data_envio
@@ -11,37 +11,35 @@ class Documento(ABC):
 
     @property
     def ide(self):
-        return self.__id
-
+        return self.__ide
     @ide.setter
-    def ide(self, id: int):
-        if isinstance(id, int): self.__id = id
+    def ide(self, nova_ide: int):
+        if isinstance(nova_ide, int): self.__ide = nova_ide
 
     @property
     def titulo(self):
         return self.__titulo
-    
     @titulo.setter
-    def titulo(self, titulo: str):
-        if isinstance(titulo, str): self.__titulo = titulo
+    def titulo(self, novo_titulo: str):
+        if isinstance(novo_titulo, str): self.__titulo = novo_titulo
 
     @property
     def descricao(self):
         return self.__descricao
-    
     @descricao.setter
-    def descricao(self, descricao: str):
-        self.__descricao = descricao
+    def descricao(self, nova_descricao: str):
+        if isinstance(nova_descricao, str): self.__descricao = nova_descricao
 
     @property
-    def data_envio(self, str):
+    def data_envio(self):
         return self.__data_envio
-    
     @data_envio.setter
-    def data_envio(self, data_envio: str):
-        self.__data_envio = data_envio
+    def data_envio(self, nova_data_envio: str):
+        if isinstance(nova_data_envio, str): self.__data_envio = nova_data_envio
 
     @property
     def autor(self):
         return self.__autor
-    
+    @autor.setter
+    def autor(self, novo_autor: Usuario):
+        if isinstance(novo_autor, Usuario): self.__autor = novo_autor
